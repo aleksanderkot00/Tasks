dad8a586f651702eb2507356d05880e00a484b0c
aleksanderkot00@gmail.com <aleksanderkot00@gmail.com>
2019-01-18 12:54:42 +0100

    Zmiany w TaskRepository

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -8,9 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Transactional
-@Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
-    @Query
+    @Override
     List<Task> findAll();
 }
