51c6a7bce23bc05a37967ca7910713b7872fd983
unknown <aleksanderkot00@gmail.com>
2019-05-09 04:50:15 +0200

    Modul 32.3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 public class MailCreatorService {
@@ -19,13 +22,21 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage Your tasks");
+        functionality.add("Provide connection with trello account");
+        functionality.add("Application allows sends tasks to trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:3000/fe");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_config", adminConfig);
         context.setVariable("goodbye_message", "Goodbye " + adminConfig.getAdminName() +  "!");
         context.setVariable("company_details", adminConfig.getCompanyDetails());
+        context.setVariable("show_button",false);
+        context.setVariable("is_friend",true);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
